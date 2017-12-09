cf68f3bf1659b3c885f485d75db9b2cbbef63ad4
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-12-09 09:30:57 -0800

    REWROTE

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 require 'json'
-def q(s)s.each{|y|y.each{|x|putc x};putc"\n"}end
-def x(s,d,o,z)l=d[0];w=d[1];h=d[2];f=E-1-h-1;e=o+l+2;j=f-w-1;q=e+w+1;u=j+h+1;(f..E-1).each do|y|(o..e-1).each do|x|if y==f||y==E-1 then g="+";i="-"else g="|";i=" "end
-s[y][x]=x==o||x==e-1 ? g:i end end
-(j..f-1).each do|y|k=o+w+1-y+j;m=k+l+1;(k..m).each do|x|if y==j then g="+";i="-"else g="/";i=" "end
-s[y][x]=x==k||x==m ? g:i end
-(o..k-1).each do|x|if y>=u && o>0 then s[y][x]="_"end end end
-(j+1..j+h).each do|y|s[y][q-1]="|"end
-(u..E-1).each do|y|k=e+w-y+u;m=k+y-j-h;r=[m+1,L].min;(k..r-1).each do|x|g=y==u ? "+":"/";i=z ? " ":"_";if !(y==E-1&&x==k)
-s[y][x]=x==k ? g:i end end end
-l+w+4+o end
-D=JSON.parse(ARGV[0]);w=D[0][1];n=D.size;L=D.map{|d|d[0]}.sum+n*w+n*4-1;E=D.map{|d|d[2]}.max+2+w+1;s=Array.new(E){Array.new(L){" "}};o=0;D.each_with_index{|d,i|o=x s,d,o,i==n-1};q s
+def B(p,q,l)a=p+l+1;(p..a).each{|x|G[q][x]=x==p||x==a ? "+":"-"}end
+def C(p,q,h)a=q+h+1;(q..a).each{|y|G[y][p]=y==q||y==a ? "+":"|"}end
+def D(p,q,w)a=q+w+1;(q..a).each{|y|G[y][p-y+q]=y==q||y==a ? "+":"/"}end
+def E(d,o,z)l=d[0];w=d[1];h=d[2];a=H-h-2;b=o+l+1;c=a-w-1;e=b+w+1;u=c+h+1;f=o+w;r=l+f+4;B(o,H-1,l);B(o,a,l);B(f+1,c,l);C(o,a,h);C(b,a,h);C(e,c,h);D(f+1,c,w);D(e,c,w);D(e,u,w);if !z then(u..u+w+1).each{|y|(e-y+u+1..r-1).each{|x|G[y][x]="_"}}end;if o>0 then(c..c+w+1).select{|y|y>=u}.each{|y|(o..f-y+c).each{|x|G[y][x]="_"}}end;r;end
+F=JSON.parse(ARGV[0])
+w=F[0][1]
+n=F.size
+L=F.map{|d|d[0]}.sum+n*w+n*4-1
+H=F.map{|d|d[2]}.max+3+w
+G=Array.new(H){Array.new(L){" "}};o=0
+F.each_with_index{|d,i|o=E d,o,i==n-1}
+G.each{|y|y.each{|x|putc x};putc"\n"}
