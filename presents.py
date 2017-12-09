c2d110765bcf2235e7170461db439bd0140b60a7
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-12-08 20:29:51 -0800

    REmoved extra

--- presents.py
+++
@@ -1,6 +0,0 @@
-from sys import argv as v
-from json import loads as l
-o=[]
-for r in l(v[2]):
-    o+=[i for i in l(v[1]) if i>=r[0] and i<=r[1]]
-print(o)
--- presents.rb
+++
@@ -1,1 +0,0 @@
-require'json';o=[];JSON.parse(ARGV[1]).each{|r|o+=JSON.parse(ARGV[0]).select{|i|i>=r[0]&&i<=r[1]}};p o
