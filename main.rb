8a2a14d3a70ad363d1133e3dcb9c66fa975dc7a4
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-12-09 21:27:48 -0800

    Removed json parsing

--- main.rb
+++ main.rb
@@ -1,12 +1,11 @@
-require 'json'
-def I(p,q)x=p;while G[q][x]==" ";G[q][x]="_";x-=1;end;end
-def C(p,q,h,f,g,b=nil)a=q+h+1;(q..a).each{|y|h=g==1 ?p:p-y+q;G[f==1 ?y:h][f==1 ?h:y]=y==q||y==a ? "+":f==1 ?g==1 ? "|":"/":"-";if y>=U&&b;I(h-1,y)end}end
-def E(d,o)l=d[0];h=d[2];a=H-h-2;b=o+l+1;c=a-W-1;e=b+W+1;f=o+W;r=l+f+4;C(H-1,o,l,0,1);C(a,o,l,0,1);C(c,f+1,l,0,1);C(o,a,h,1,1,o>0);C(b,a,h,1,1);C(e,c,h,1,1);C(f+1,c,W,1,0,o>0);C(e,c,W,1,0);C(e,U,W,1,0);r;end
-f=JSON.parse(ARGV[0])
-W=f[0][1]
+def O(f)
+def I(p,q)x=p;while $s[q][x]==" ";$s[q][x]="_";x-=1;end;end
+def C(p,q,h,f,g,b=nil)a=q+h+1;(q..a).each{|y|h=g==1 ?p:p-y+q;$s[f==1 ?y:h][f==1 ?h:y]=y==q||y==a ? "+":f==1 ?g==1 ? "|":"/":"-";if y>=$u&&b;I(h-1,y)end}end
+def E(d,o,w,z)l=d[0];h=d[2];a=z-h-2;b=o+l+1;c=a-w-1;e=b+w+1;f=o+w;r=l+f+4;C(z-1,o,l,0,1);C(a,o,l,0,1);C(c,f+1,l,0,1);C(o,a,h,1,1,o>0);C(b,a,h,1,1);C(e,c,h,1,1);C(f+1,c,w,1,0,o>0);C(e,c,w,1,0);C(e,$u,w,1,0);r;end
+w=f[0][1]
 n=f.size
-H=f.map{|d|d[2]}.max+3+W
-U=H-2-W
-x=Array;G=x.new(H){x.new(f.map{|d|d[0]}.sum+n*W+n*4-1){" "}};o=0
-f.each{|d|o=E d,o}
-puts G.map{|y|y.join()}
+z=f.map{|d|d[2]}.max+3+w
+$u=z-2-w
+x=Array;$s=x.new(z){x.new(f.map{|d|d[0]}.sum+n*w+n*4-1){" "}};o=0
+f.each{|d|o=E d,o,w,z}
+puts $s.map{|y|y.join()}end
